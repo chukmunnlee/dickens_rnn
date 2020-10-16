d4f1df281093725e9c335f0fe1bff90ba338f292
Chuk <chukmunnlee@gmail.com>
2020-10-16 16:45:25 +0800

    still tokenizing

--- 0_tokenize.py
+++ 0_tokenize.py
@@ -1,15 +1,19 @@
-import pickle
 import spacy
 import numpy as np
 import tensorflow as tf
+import pickle
+import re
 
 from util import *
 from tqdm import *
 
 from tensorflow.keras.preprocessing.text import Tokenizer
 
-tokenizer = Tokenizer(num_words=200000, lower=True,
-      filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n—’‘“”')
+#filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n—’‘“”'
+filters='!"#$%&()*+,-./:;<=>?@[\\]^_{|}~\t\n—“”'
+F = '!"#$%&\(\)\*\+,-\./:;<=>\?@\[\]\\\^_`\{\}|\~\t\n“”'
+
+tokenizer = Tokenizer(num_words=200000, lower=True, char_level=False, filters=filters)
 
 nlp = spacy.load('en_core_web_lg')
 
@@ -21,18 +25,19 @@ LINES = len(text_chunks)
 
 for i in tqdm(range(LINES)):
    #line = tokenize_line(text_chunks[i])
-   line = text_chunks[i]
+   line = text_chunks[i].strip().lower()
+   line = re.sub(F, ' ', line)
    if len(line) <= 0:
       continue
+   line = tokenize_line(line)
    tokenizer.fit_on_texts(line)
-   seq = tokenizer.texts_to_sequences([ line ])
-   tok = tokenizer.sequences_to_texts(seq)
-
-   print('seq = ', seq)
-   print('tok = ', tok)
+   seq = tokenizer.texts_to_sequences(line)
+   txt = tokenizer.sequences_to_texts(seq)
+   toks = nlp(' '.join(txt))
+   for s, t in zip(seq, toks):
+      print(t, t.pos, s[0])
+   
    exit()
-
-
 with open('./tokenizer.pickle', 'wb') as f:
    pickle.dump(tok, f)
 
